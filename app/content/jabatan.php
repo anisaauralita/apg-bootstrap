d4ead6cc5ff641bd350433b8d7012ac86a73af6c
Anisa Auralita Aszahra <anisaauralita20@gmail.com>
2025-02-06 00:34:34 +0700

    Upload code

--- app/content/jabatan.php
+++ app/content/jabatan.php
@@ -1,36 +1,34 @@
 <?php
 if(!defined('INDEX')) die("");
 ?>
-
-<h2 class="judul">Data Jabatan</h2>
-<a href="?hal=jabatan_tambah" class="tombol">Tambah</a>
-
-<table class="table">
-    <thead>
-        <tr>
-            <th>No</th>
-            <th>Nama Jabatan</th>
-            <th>Aksi</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        $query = "SELECT * FROM jabatan ORDER BY id_jabatan DESC";
-        $result = mysqli_query($con,$query);
-        $no = 0;
-        while($data = mysqli_fetch_array($result)){
-            $no++;
-        ?>
+<h4 class="mt-2">Data Jabatan</h4>
+<hr>
+<a class="btn btn-success" href="?hal=jabatan_tambah"><i class="oi oi-plus"></i>Tambah</a>
+<div class="table-responsive">
+    <table class="table table-striped table-hover table-bordered">
+        <thead>
             <tr>
-                <td><?= $no ?></td>
-                <td><?= $data['nama_jabatan'] ?></td>
-                <td>
-                    <a href="?hal=jabatan_edit&id=<?= $data['id_jabatan'] ?>" class="tombol edit"> Edit</a>
-                    <a href="?hal=jabatan_hapus&id=<?= $data['id_jabatan'] ?>" class="tombol hapus"> Hapus</a>
-                </td>
+                <th>No</th>
+                <th>Nama Jabatan</th>
+                <th>Aksi</th>
             </tr>
-        <?php    
-        }
-        ?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            <?php
+            $query = mysqli_query($con, "SELECT * FROM jabatan ORDER BY id_jabatan DECS");
+            $no = 0;
+            while($data = mysqli_fetch_array($query)){
+                $no++;
+            ?>
+                <tr>
+                    <td><?= $no ?></td>
+                    <td><?= $data['nama_jabatan'] ?></td>
+                    <td>
+                        <a class="btn btn-sm btn-info" href="?hal=jabatan_edit&id=<?= $data['id_jabatan'] ?>"><i class="oi oi-pencil"></i>Edit</a>
+                    </td>
+                </tr>
+            }
+            ?>
+        </tbody>
+    </table>
+</div>
